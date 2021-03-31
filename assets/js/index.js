5ce65781580ff6e19764b69c121e969e16e135e4
lorcents <iannjoroge120@outlook.com>
2021-03-31 22:32:57 +0300

    changes2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,25 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function googleSignin() {
+    firebase.auth()
+
+    .signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        console.log(token)
+        console.log(user)
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error.code)
+        console.log(error.message)
+    });
+}
+
+
+
 function login() {
     var userEmail = document.getElementById("email-auth").value;
     var userPass = document.getElementById("pass-auth").value;
